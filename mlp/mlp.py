9f5f6c225fb4d6b05552b17187ee6042457533cf
Abdelkader Benamara <ga_benamara@esi.dz>
2022-04-12 13:00:41 +0200

    [MLP] -- create revort

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -1,5 +1,6 @@
 # imports
 import os
+from unicodedata import name
 import tensorflow.nn as nn
 from tensorflow.keras import Input,Model
 from tensorflow.keras.utils import plot_model
@@ -17,7 +18,7 @@ config =    {   'arch' : {
                             '2L_64x1'    : [64,64],
                             '3L_32x2'    : [32,64,128],
                             '4L_128x0.5' : [128,64,32,16] 
-                        }                
+                        },           
                 'n_nodes'     : 64,
                 'factor'        : 2,
                 'n_layers'  : 2,
@@ -34,15 +35,14 @@ config =    {   'arch' : {
 '''
 class FNC(object):
     
-    def __init__(self,arch='3L_32x2',n_nodes=config['n_nodes'],factor=config['factor'],
-                n_layers=config['n_layers'],l2_reg=config['l2_reg'],dropout=config['dropout']
-                dim=20672) -> None:
+    def __init__(self,X_train,y_train,arch='3L_32x2',n_nodes=config['n_nodes'],factor=config['factor'],
+                n_layers=config['n_layers'],l2_reg=config['l2_reg'],dropout=config['dropout']) -> None:
         super().__init__()
+        self.X_train , self.y_train  = X_train , y_train
         self.arch = arch
         self.n_nodes = n_nodes
         self.factor = factor
         self.n_layers = n_layers
-        self.dim = dim
         self.l2_reg = regularizers.l2(l2_reg)
         self.dropout = dropout
         self.model = None
@@ -64,7 +64,7 @@ class FNC(object):
         
     def build_model(self,n_blocks=config.n_res_blocks):
         # Input Block
-        inp = Input(shape=(self.dim, 1), name='Input')
+        inp = Input(shape=(self.X_train.shape[1], 1), name='Input')
 
         # Body Block 
         x = self.body_block(x,n_blocks)
@@ -87,12 +87,14 @@ class FNC(object):
         n = self.n_nodes
         f = 1
         for i in range(self.n_layers):
-            x = layers.Dense(n*f,kernel_regularizer=self.l2_reg)(x)
+            x = layers.Dense(n*f,kernel_regularizer=self.l2_re,name=f"Layer #{i+1}")(x)
             x = self.activation(x)
         return x
         
     def output_block(self,x):
-        return layers.Activation('softmax', name='Output')(layers.Flatten()(x))
+        x = layers.Flatten()(x)
+        x = layers.Dense(self.y_train.shape[1],activation ='softmax',name='Output')
+        return x
 
     def activation(self,x):
         return nn.relu(x)
